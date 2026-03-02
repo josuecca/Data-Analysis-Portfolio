e847eef6b88846733843aeb48d0fc6a53583d89b
Josué-Carmona-Data-Analyst <120145276+josuecca@users.noreply.github.com>
2026-03-02 13:53:51 -0600

    Queries.sql

--- austin-bikeshare-sql/queries.sql
+++ austin-bikeshare-sql/queries.sql
@@ -1,27 +1,49 @@
-WITH longest_used_bike AS (
+WITH bike_usage AS (
   SELECT
     bike_id,
-    SUM(duration_minutes) AS trip_duration 
+    SUM(duration_minutes) AS total_duration,
+    COUNT(*) AS total_trips
   FROM
     bigquery-public-data.austin_bikeshare.bikeshare_trips
+  WHERE
+    start_time BETWEEN '2019-01-01' AND '2019-12-31'
   GROUP BY
     bike_id
+),
+
+longest_used_bike AS (
+  SELECT *
+  FROM bike_usage
+  ORDER BY total_duration DESC
+  LIMIT 1
+),
+
+station_frequency AS (
+  SELECT
+    trips.start_station_id,
+    COUNT(*) AS trip_ct
+  FROM
+    longest_used_bike AS longest
+  JOIN
+    bigquery-public-data.austin_bikeshare.bikeshare_trips AS trips
+  ON
+    longest.bike_id = trips.bike_id
+  WHERE
+    trips.start_time BETWEEN '2019-01-01' AND '2019-12-31'
+  GROUP BY
+    trips.start_station_id
   ORDER BY
-    trip_duration DESC 
+    trip_ct DESC
   LIMIT 1
 )
 
 SELECT
-  trips.start_station_id,
-  COUNT(*) AS trip_ct
+  sf.start_station_id,
+  stations.name AS station_name,
+  sf.trip_ct
 FROM
-  longest_used_bike AS longest 
-INNER JOIN
-  bigquery-public-data.austin_bikeshare.bikeshare_trips AS trips
+  station_frequency sf
+JOIN
+  bigquery-public-data.austin_bikeshare.bikeshare_stations stations
 ON
-  longest.bike_id = trips.bike_id
-GROUP BY
-  trips.start_station_id
-ORDER BY 
-  trip_ct DESC
-LIMIT 1;
+  sf.start_station_id = stations.station_id;
